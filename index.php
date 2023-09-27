be9537188f415e51d1781d7b92a2bcbe7fc05cce
kevinbogonko <61062173+kevinbogonko@users.noreply.github.com>
2023-09-27 12:27:47 +0000

    final for today

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <form action="action.php" method="post">
+            <input type="text" name="text" id="text">
+            <input type="submit" value="Check Input">
+        </form>
+    </div>
+</body>
+</html>
